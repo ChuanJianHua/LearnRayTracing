bf99b3a7c8eba16f51e84ddb4c466c60e15da148
huajian <1415885403@qq.com>
2019-06-18 01:23:38 +0800

    add vec3 and ray class

--- LeranRayTracing/LeranRayTracing/helloGraphic.cpp
+++ LeranRayTracing/LeranRayTracing/helloGraphic.cpp
@@ -1,7 +1,17 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include "vec3.h"
+#include "ray.h"
 using namespace std;
+
+vec3 color(const ray& r)
+{
+	vec3 unit_direction = unit_vector(r.direction());
+	auto t = 0.5*(unit_direction.y() + 1.0);
+	return (1.0 - t) * vec3(1.0, 1.0, 1.0) + t * vec3(0.5, 0.7, 1.0);
+}
+
 
 int main()
 {
@@ -11,16 +21,26 @@ int main()
 	ofstream outFile;
 	outFile.open(fileName);
 	outFile << "P3\n" << nx << " " << ny << "\n255\n";
+
+	vec3 lower_left_corner(-2.0, -1.0, -1.0);
+
+	vec3 horizontal(4.0, 2.0, 0.0);
+	vec3 vertical(0.0, 2.0, 0.0);
+	vec3 origin(0.0, 0.0, 0.0);
 	for (int j = ny-1; j >= 0; j--)
 	{
 		for (int i = 0; i < nx; i++)
 		{
-			float r = static_cast<float>(i) / static_cast<float>(nx);
-			float g = static_cast<float>(j) / static_cast<float>(ny);
+			float u = static_cast<float>(i) / static_cast<float>(nx);
+			float v = static_cast<float>(j) / static_cast<float>(ny);
+
+			ray r(origin, lower_left_corner + u * horizontal + v * vertical);
+			vec3 col = color(r);
+
 			float b = 0.2f;
-			int ir = int(255.99*r);
-			int ig = int(255.99*g);
-			int ib = int(255.99*b);
+			int ir = int(255.99*col[0]);
+			int ig = int(255.99*col[1]);
+			int ib = int(255.99*col[2]);
 			outFile << ir << " " << ig << " " << ib << "\n";
 		}
 	}
